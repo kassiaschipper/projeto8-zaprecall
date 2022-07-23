3688b2b289e1679a07b696689c6b5be2e71626e0
Kássia Guterres Schipper <78599273+kassiaschipper@users.noreply.github.com>
2022-07-22 21:00:54 -0300

    Update Answer.js

--- src/Answer.js
+++ src/Answer.js
@@ -1,16 +1,7 @@
 import React from "react";
 
 export default function Answer() {
-//  function options() {
-//      SetFlahscardState("answered");
-//      alert("mostra resposta");
-//    }
-   
-//  function answerState(){
-//     setAnswer(zapStatus);
-//     setImgAnswers([...icons, iconName]);
-//     setImgAnswer(iconName);
-// }
+
   
 
   return (
